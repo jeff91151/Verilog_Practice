edd301b5b26d4f48e1552cc426429dc12187b206
jeff91151 <71751559+jeff91151@users.noreply.github.com>
2024-07-15 16:52:20 +0800

    Update Exams ece241 2014 q5a.v

--- FSM/Exams ece241 2014 q5a - 未完成.v
+++ FSM/Exams ece241 2014 q5a - 未完成.v
@@ -4,44 +4,35 @@ module top_module (
     input x,
     output z
 ); 
-    
     parameter A = 0, B = 1, C = 2, D = 3;
     reg [1:0] state, next_state;
-
+ 
     always @(*) begin
-        
         case (state)
-            
             A: next_state = x ? B : A;
             B: next_state = x ? C : D;
             C: next_state = x ? C : D;
             D: next_state = x ? D : C;
-            
         endcase
-        
     end
     always @(posedge clk or posedge areset) begin
-        
         if (areset) begin
            state <= A; 
         end else begin
-            
            state <= next_state; 
         end
-        
     end
-    always @(*) begin
-        
-        if (state == A) begin
-           z = x; 
+    always @(posedge clk or posedge areset) begin
+        if (areset) begin
+           z <= 0; 
+        end else if (state == A) begin
+           z <= x; 
         end else if (state == B) begin
-            
-           z = ~x; 
+           z <= ~x; 
         end else if (state == C) begin
-           z = ~x; 
+           z <= ~x;
         end else if (state == D) begin
-            
+           z <= ~x;
         end
     end
-    
 endmodule
